38bb25d8138d059bf2db134325ee553c41207c7d
Or Bruchim <orbruhim@gmail.com>
2022-11-25 00:00:59 +0200

    add chip for sorting

--- chip/chip.component.ts
+++ chip/chip.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'talon-assigment-chip',
   templateUrl: './chip.component.html',
-  styleUrls: ['./chip.component.css']
+  styleUrls: ['./chip.component.css'],
 })
 export class ChipComponent implements OnInit {
+  @Input() filterValue: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
