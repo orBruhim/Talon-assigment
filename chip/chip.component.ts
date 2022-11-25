0dbb926bc92ed3e71c405b94a2f4124806cc476a
Or Bruchim <orbruhim@gmail.com>
2022-11-25 16:17:07 +0200

    add chip

--- chip/chip.component.ts
+++ chip/chip.component.ts
@@ -3,12 +3,18 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'talon-assigment-chip',
   templateUrl: './chip.component.html',
-  styleUrls: ['./chip.component.css'],
+  styleUrls: ['./chip.component.scss'],
 })
 export class ChipComponent implements OnInit {
   @Input() filterValue: string = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.filterValue);
+  }
+
+  cancelFilter() {
+    console.log(this.filterValue);
+  }
 }
